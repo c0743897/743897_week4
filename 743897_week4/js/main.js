3ae42fdadb85e8c4bb58bd93d957c27dc49f4aca
visualstudio <visualstudio@230-PC08>
2019-01-31 11:45:18 -0500

    Create function and loaded it with body onload event

--- 743897_week4/js/main.js
+++ 743897_week4/js/main.js
@@ -1,1 +1,6 @@
-﻿// Your code here!
+﻿// Your code here! 
+function myFunction(){
+var date = new Date();
+
+document.body.innerHTML = "<h1>The date is " + (date.getMonth() + 1) + " / " + date.getDate() + " / " + date.getFullYear() + " </h1>";
+}
